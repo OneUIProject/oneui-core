d5785dbb160557890c1596203713aaf345b49634
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-03-18 09:05:34 +0100

    lib: coordinatorlayout: burp
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- coordinatorlayout/src/main/java/androidx/coordinatorlayout/widget/CoordinatorLayout.java
+++ coordinatorlayout/src/main/java/androidx/coordinatorlayout/widget/CoordinatorLayout.java
@@ -321,7 +321,7 @@ public class CoordinatorLayout extends ViewGroup implements NestedScrollingParen
                 final int childCount = getChildCount();
                 for (int i = 0; i < childCount; i++) {
                     final View child = getChildAt(i);
-                    
+
                     if (child instanceof AppBarLayoutBehavior) {
                         AppBarLayoutBehavior behavior = (AppBarLayoutBehavior) child;
                         if (!behavior.seslIsCollapsed()) {

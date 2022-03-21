642d5462f3f2d7b351d4bbdf765df1b71ca43e4e
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-03-21 18:21:07 +0100

    lib: recyclerview: RecyclerView: minor adjustments
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- recyclerview/src/main/java/androidx/recyclerview/widget/RecyclerView.java
+++ recyclerview/src/main/java/androidx/recyclerview/widget/RecyclerView.java
@@ -4080,175 +4080,173 @@ public class RecyclerView extends ViewGroup implements ScrollingView,
         final MotionEvent vtev = MotionEvent.obtain(e);
         vtev.offsetLocation(mNestedOffsets[0], mNestedOffsets[1]);
 
-        if (mFastScroller == null || !mFastScroller.onTouchEvent(e)) {
-            switch (action) {
-                case MotionEvent.ACTION_DOWN: {
-                    mScrollPointerId = e.getPointerId(0);
-                    mInitialTouchX = mLastTouchX = (int) (e.getX() + 0.5f);
-                    mInitialTouchY = mLastTouchY = (int) (e.getY() + 0.5f);
-
-                    if (mHasNestedScrollRange) {
-                        adjustNestedScrollRange();
-                    }
-
-                    int nestedScrollAxis = ViewCompat.SCROLL_AXIS_NONE;
-                    if (canScrollHorizontally) {
-                        nestedScrollAxis |= ViewCompat.SCROLL_AXIS_HORIZONTAL;
-                    }
-                    if (canScrollVertically) {
-                        nestedScrollAxis |= ViewCompat.SCROLL_AXIS_VERTICAL;
-                    }
-                    startNestedScroll(nestedScrollAxis, TYPE_TOUCH);
-                }
-                break;
-
-                case MotionEvent.ACTION_POINTER_DOWN: {
-                    mScrollPointerId = e.getPointerId(actionIndex);
-                    mInitialTouchX = mLastTouchX = (int) (e.getX(actionIndex) + 0.5f);
-                    mInitialTouchY = mLastTouchY = (int) (e.getY(actionIndex) + 0.5f);
-                }
-                break;
-
-                case MotionEvent.ACTION_MOVE: {
-                    final int index = e.findPointerIndex(mScrollPointerId);
-                    if (index < 0) {
-                        Log.e(TAG, "Error processing scroll; pointer index for id "
-                                + mScrollPointerId + " not found. Did any MotionEvents get skipped?");
-                        return false;
-                    }
-
-                    final int x = (int) (e.getX(index) + 0.5f);
-                    final int y = (int) (e.getY(index) + 0.5f);
-                    int dx = mLastTouchX - x;
-                    int dy = mLastTouchY - y;
-
-                    if (mScrollState != SCROLL_STATE_DRAGGING) {
-                        boolean startScroll = false;
-                        if (canScrollHorizontally) {
-                            if (dx > 0) {
-                                dx = Math.max(0, dx - mTouchSlop);
-                            } else {
-                                dx = Math.min(0, dx + mTouchSlop);
-                            }
-                            if (dx != 0) {
-                                startScroll = true;
-                            }
-                        }
-                        if (canScrollVertically) {
-                            if (dy > 0) {
-                                dy = Math.max(0, dy - mTouchSlop);
-                            } else {
-                                dy = Math.min(0, dy + mTouchSlop);
-                            }
-                            if (dy != 0) {
-                                startScroll = true;
-                            }
-                        }
-                        if (startScroll) {
-                            setScrollState(SCROLL_STATE_DRAGGING);
-                        }
-                    }
-
-                    if (mScrollState == SCROLL_STATE_DRAGGING) {
-                        mReusableIntPair[0] = 0;
-                        mReusableIntPair[1] = 0;
-                        dx -= releaseHorizontalGlow(dx, e.getY());
-                        dy -= releaseVerticalGlow(dy, e.getX());
-
-                        if (dispatchNestedPreScroll(
-                                canScrollHorizontally ? dx : 0,
-                                canScrollVertically ? dy : 0,
-                                mReusableIntPair, mScrollOffset, TYPE_TOUCH
-                        )) {
-                            dx -= mReusableIntPair[0];
-                            dy -= mReusableIntPair[1];
-                            // Updated the nested offsets
-                            mNestedOffsets[0] += mScrollOffset[0];
-                            mNestedOffsets[1] += mScrollOffset[1];
-                            // Scroll has initiated, prevent parents from intercepting
-                            getParent().requestDisallowInterceptTouchEvent(true);
-                        }
-
-                        mLastTouchX = x - mScrollOffset[0];
-                        mLastTouchY = y - mScrollOffset[1];
-
-                        if ((e.getFlags() & mMotionEventUpPendingFlag) != 0) {
-                            mVelocityTracker.addMovement(vtev);
-                            mIsSkipMoveEvent = true;
-                            return false;
-                        }
-
-                        if (!mGoToTopMoved) {
-                            if (scrollByInternal(
-                                    canScrollHorizontally ? dx : 0,
-                                    canScrollVertically ? dy : 0,
-                                    e, TYPE_TOUCH)) {
-                                getParent().requestDisallowInterceptTouchEvent(true);
-                            }
-                        }
-                        if (mGapWorker != null && (dx != 0 || dy != 0)) {
-                            mGapWorker.postFromTraversal(this, dx, dy);
-                        }
-                    }
-                }
-                break;
-
-                case MotionEvent.ACTION_POINTER_UP: {
-                    onPointerUp(e);
-                }
-                break;
-
-                case MotionEvent.ACTION_UP: {
-                    mVelocityTracker.addMovement(vtev);
-                    eventAddedToVelocityTracker = true;
-                    mVelocityTracker.computeCurrentVelocity(1000, mMaxFlingVelocity);
-                    final float xvel = canScrollHorizontally
-                            ? -mVelocityTracker.getXVelocity(mScrollPointerId) : 0;
-                    final float yvel = canScrollVertically
-                            ? -mVelocityTracker.getYVelocity(mScrollPointerId) : 0;
-                    if (!((xvel != 0 || yvel != 0) && fling((int) xvel, (int) yvel))) {
-                        setScrollState(SCROLL_STATE_IDLE);
-                    }
-                    Log.i(TAG, "onTouchUp() velocity : " + yvel
-                            + ", last move skip : " + mIsSkipMoveEvent
-                            + ", use scroller : " + mViewFlinger.mOverScroller.getClass().getName());
-                    resetScroll();
-                }
-                break;
-
-                case MotionEvent.ACTION_CANCEL: {
-                    cancelScroll();
-                }
-                break;
-            }
-
-            if (!eventAddedToVelocityTracker) {
-                mVelocityTracker.addMovement(vtev);
-            }
-            vtev.recycle();
-
-            return true;
-        }
-
-        if (mFastScrollerEventListener != null) {
-            switch (e.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN:
-                case MotionEvent.ACTION_MOVE:
+        if (mFastScroller != null && mFastScroller.onTouchEvent(e)) {
+            if (mFastScrollerEventListener != null) {
+                if (e.getActionMasked() == MotionEvent.ACTION_DOWN
+                        || e.getActionMasked() == MotionEvent.ACTION_MOVE) {
                     if (mFastScroller.getEffectState()
                             == SeslRecyclerViewFastScroller.EFFECT_STATE_OPEN) {
                         mFastScrollerEventListener.onPressed(mFastScroller.getScrollY());
                     }
-                    break;
-                case MotionEvent.ACTION_UP:
+                }
+                if (e.getActionMasked() == MotionEvent.ACTION_UP) {
                     if (mFastScroller.getEffectState()
                             == SeslRecyclerViewFastScroller.EFFECT_STATE_CLOSE) {
                         mFastScrollerEventListener.onReleased(mFastScroller.getScrollY());
                     }
-                    break;
+                }
             }
+
+            vtev.recycle();
+            return true;
         }
 
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                mScrollPointerId = e.getPointerId(0);
+                mInitialTouchX = mLastTouchX = (int) (e.getX() + 0.5f);
+                mInitialTouchY = mLastTouchY = (int) (e.getY() + 0.5f);
+
+                if (mHasNestedScrollRange) {
+                    adjustNestedScrollRange();
+                }
+
+                int nestedScrollAxis = ViewCompat.SCROLL_AXIS_NONE;
+                if (canScrollHorizontally) {
+                    nestedScrollAxis |= ViewCompat.SCROLL_AXIS_HORIZONTAL;
+                }
+                if (canScrollVertically) {
+                    nestedScrollAxis |= ViewCompat.SCROLL_AXIS_VERTICAL;
+                }
+                startNestedScroll(nestedScrollAxis, TYPE_TOUCH);
+            }
+            break;
+
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                mScrollPointerId = e.getPointerId(actionIndex);
+                mInitialTouchX = mLastTouchX = (int) (e.getX(actionIndex) + 0.5f);
+                mInitialTouchY = mLastTouchY = (int) (e.getY(actionIndex) + 0.5f);
+            }
+            break;
+
+            case MotionEvent.ACTION_MOVE: {
+                final int index = e.findPointerIndex(mScrollPointerId);
+                if (index < 0) {
+                    Log.e(TAG, "Error processing scroll; pointer index for id "
+                            + mScrollPointerId + " not found. Did any MotionEvents get skipped?");
+                    return false;
+                }
+
+                final int x = (int) (e.getX(index) + 0.5f);
+                final int y = (int) (e.getY(index) + 0.5f);
+                int dx = mLastTouchX - x;
+                int dy = mLastTouchY - y;
+
+                if (mScrollState != SCROLL_STATE_DRAGGING) {
+                    boolean startScroll = false;
+                    if (canScrollHorizontally) {
+                        if (dx > 0) {
+                            dx = Math.max(0, dx - mTouchSlop);
+                        } else {
+                            dx = Math.min(0, dx + mTouchSlop);
+                        }
+                        if (dx != 0) {
+                            startScroll = true;
+                        }
+                    }
+                    if (canScrollVertically) {
+                        if (dy > 0) {
+                            dy = Math.max(0, dy - mTouchSlop);
+                        } else {
+                            dy = Math.min(0, dy + mTouchSlop);
+                        }
+                        if (dy != 0) {
+                            startScroll = true;
+                        }
+                    }
+                    if (startScroll) {
+                        setScrollState(SCROLL_STATE_DRAGGING);
+                    }
+                }
+
+                if (mScrollState == SCROLL_STATE_DRAGGING) {
+                    mReusableIntPair[0] = 0;
+                    mReusableIntPair[1] = 0;
+                    dx -= releaseHorizontalGlow(dx, e.getY());
+                    dy -= releaseVerticalGlow(dy, e.getX());
+
+                    if (dispatchNestedPreScroll(
+                            canScrollHorizontally ? dx : 0,
+                            canScrollVertically ? dy : 0,
+                            mReusableIntPair, mScrollOffset, TYPE_TOUCH
+                    )) {
+                        dx -= mReusableIntPair[0];
+                        dy -= mReusableIntPair[1];
+                        // Updated the nested offsets
+                        mNestedOffsets[0] += mScrollOffset[0];
+                        mNestedOffsets[1] += mScrollOffset[1];
+                        // Scroll has initiated, prevent parents from intercepting
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                    }
+
+                    mLastTouchX = x - mScrollOffset[0];
+                    mLastTouchY = y - mScrollOffset[1];
+
+                    if ((e.getFlags() & mMotionEventUpPendingFlag) != 0) {
+                        mVelocityTracker.addMovement(vtev);
+                        mIsSkipMoveEvent = true;
+                        return false;
+                    }
+
+                    if (!mGoToTopMoved) {
+                        if (scrollByInternal(
+                                canScrollHorizontally ? dx : 0,
+                                canScrollVertically ? dy : 0,
+                                e, TYPE_TOUCH)) {
+                            getParent().requestDisallowInterceptTouchEvent(true);
+                        }
+                    }
+                    if (mGapWorker != null && (dx != 0 || dy != 0)) {
+                        mGapWorker.postFromTraversal(this, dx, dy);
+                    }
+                }
+            }
+            break;
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                onPointerUp(e);
+            }
+            break;
+
+            case MotionEvent.ACTION_UP: {
+                mVelocityTracker.addMovement(vtev);
+                eventAddedToVelocityTracker = true;
+                mVelocityTracker.computeCurrentVelocity(1000, mMaxFlingVelocity);
+                final float xvel = canScrollHorizontally
+                        ? -mVelocityTracker.getXVelocity(mScrollPointerId) : 0;
+                final float yvel = canScrollVertically
+                        ? -mVelocityTracker.getYVelocity(mScrollPointerId) : 0;
+                if (!((xvel != 0 || yvel != 0) && fling((int) xvel, (int) yvel))) {
+                    setScrollState(SCROLL_STATE_IDLE);
+                }
+                Log.i(TAG, "onTouchUp() velocity : " + yvel
+                        + ", last move skip : " + mIsSkipMoveEvent
+                        + ", use scroller : " + mViewFlinger.mOverScroller.getClass().getName());
+                resetScroll();
+            }
+            break;
+
+            case MotionEvent.ACTION_CANCEL: {
+                cancelScroll();
+            }
+            break;
+        }
+
+        if (!eventAddedToVelocityTracker) {
+            mVelocityTracker.addMovement(vtev);
+        }
         vtev.recycle();
+
         return true;
     }
 
